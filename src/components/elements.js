4fc62af35eefddc11847544a1c29211c50f80d3e
Edward Glendinning <edward82009@hotmail.com>
2022-01-05 20:12:59 +0000

    Add responsive styling for small screens

--- src/components/elements.js
+++ src/components/elements.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
 export const ContainerDiv = styled.div`
+    @media screen and (max-width: 1100px) {
+        grid-template-columns: 5% auto 5%;
+    }
     display: grid;
-    grid-template-columns: 1fr 2fr 1fr;
+    grid-template-columns: 15% auto 15%;
     z-index: 1000;
     height: max-content;
 `;
@@ -13,11 +16,15 @@ export const HeaderRow = styled.div`
     text-align: center;
     background-color: ${(props) => props.theme.foregroundCol};
     z-index: 1;
-    border-top-left-radius: 5px;
-    border-top-right-radius: 5px;
+    border-radius: 5px;
+    margin-bottom: 10px;
 `;
 
 export const ModeButtonContainer = styled.div`
+    @media screen and (max-width: 1100px) {
+        grid-column: 2;
+        grid-row: auto;
+    }
     grid-column: 3;
     grid-row: 1;
     text-align: center;
@@ -29,9 +36,9 @@ export const ContentArea = styled.div`
     grid-row: 2;
     background-color: ${(props) => props.theme.foregroundCol};
     z-index: 1;
-    padding: 10px;
-    border-bottom-left-radius: 5px;
-    border-bottom-right-radius: 5px;
+    padding: 0 10px;
+    border-radius: 5px;
+    margin-bottom: 10px;
 `;
 
 export const ProjectList = styled.ul`
